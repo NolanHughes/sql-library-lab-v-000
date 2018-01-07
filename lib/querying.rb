606ee5dd238511a700fc44fa86a9b8534e82fb5c
Nolan Hughes <nhughes987@gmail.com>
2018-01-07 20:28:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,13 +20,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  # " SELECT Series.title
-  #   FROM series
-  #   JOIN characters
-  #   ON series.id = characters.series_id
-  #   WHERE characters.species = 'human'
-  #   GROUP BY series.title
-  #   ORDER BY COUNT(species);"
+  " SELECT Series.title
+    FROM series
+    JOIN characters
+    ON series.id = characters.series_id
+    WHERE characters.species = 'human'
+    GROUP BY series.title
+    ORDER BY COUNT(*) DESC
+    LIMIT 1;"
 
     " SELECT Series.title
       FROM characters

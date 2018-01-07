b9ffe73796fce5e4442387ee0321f6b903927f4f
Nolan Hughes <nhughes987@gmail.com>
2018-01-07 20:30:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,21 +28,6 @@ def select_series_title_with_most_human_characters
     GROUP BY Series.title
     ORDER BY COUNT(*) DESC
     LIMIT 1;"
-
-    # " SELECT Series.title
-    #   FROM characters
-    #   LEFT OUTER JOIN series
-    #   WHERE species = 'human'
-    #   LIMIT 1;"
-    #
-    #   SELECT series.title
-    #   FROM series
-    #   JOIN characters
-    #   ON character_books.character_id = characters.id
-    #   WHERE characters.species = 'human'
-    #   GROUP BY series.title
-    #   ORDER BY COUNT(*) DESC
-    #   LIMIT 1
 end
 
 def select_character_names_and_number_of_books_they_are_in
